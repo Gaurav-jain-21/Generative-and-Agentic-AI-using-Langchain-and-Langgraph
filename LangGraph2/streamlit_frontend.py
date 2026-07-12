4d4e3f13e36b01135a64e9afbc4c3ceb7e95b2db
Gaurav jain <gjain7764@gmail.com>
2026-07-12 22:01:26 +0530

    this is my commit

---
+++ LangGraph2/streamlit_frontend.py
@@ -0,0 +1,16 @@
+import streamlit as st
+message_history=[]
+
+for message in message_history:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
+user_input= st.chat_input("Type here")
+
+if user_input: 
+    message_history.append({'role': 'user', 'content':user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
+
+    message_history.append({'role':'assistant','content':user_input})
+    with st.chat_message('assistant'):
+        st.text(user_input)
