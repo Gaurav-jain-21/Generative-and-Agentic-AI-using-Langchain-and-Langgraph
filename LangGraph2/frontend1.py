c393bd9e51b8102a16d2dd7065208753f2de6625
Gaurav jain <gjain7764@gmail.com>
2026-07-17 14:26:08 +0530

    this is my commmit

--- LangGraph2/frontend1.py
+++ LangGraph2/frontend1.py
@@ -1,37 +1,47 @@
 import streamlit as st
 from langgraph_backend import chatbot
 from langchain_core.messages import HumanMessage
+import uuid
 
-CONFIG = {'configurable': {'thread_id': 'thread-1'}}
+def generate_thread_id():
+    thread_id=uuid.uuid4()
+    return thread_id;
+
+
 
 if 'message_history' not in st.session_state:
     st.session_state['message_history'] = []
 
+if 'thread_id' not in st.session_state:
+    st.session_state['thread_id']=generate_thread_id()
 
+
+st.sidebar.title("LangGraph Chatbot")
+st.sidebar.button("New Chat")
+st.sidebar.header("My Conversations")
+
+st.sidebar.text(st.session_state['thread_id'])
 
 for message in st.session_state['message_history']:
     with st.chat_message(message['role']):
         st.text(message['content'])
 
-#{'role': 'user', 'content': 'Hi'}
-#{'role': 'assistant', 'content': 'Hi=ello'}
 
 user_input = st.chat_input('Type here')
 
 if user_input:
 
-    # first add the message to message_history
     st.session_state['message_history'].append({'role': 'user', 'content': user_input})
     with st.chat_message('user'):
         st.text(user_input)
 
-    # first add the message to message_history
+    CONFIG = {'configurable': {'thread_id': st.session_state['thread_id']}}
     with st.chat_message('assistant'):
 
         ai_message = st.write_stream(
             message_chunk.content for message_chunk, metadata in chatbot.stream(
                 {'messages': [HumanMessage(content=user_input)]},
-                config= {'configurable': {'thread_id': 'thread-1'}},
+                config= CONFIG,
                 stream_mode= 'messages'
             )
         )
