2f7bc490fe7a3345f656b50f4e68008ae792ef6a
Gaurav jain <gjain7764@gmail.com>
2026-07-20 22:40:38 +0530

    this is called a web seach

--- LastClass/pdfloader.py
+++ LastClass/pdfloader.py
@@ -1,5 +1,33 @@
-from langchain_community.document_loaders import PyPDFLoader
-loader= PyPDFLoader('dl-curriculum.pdf')
-doc= loader.load()
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-print(doc[0].page_content)
+load_dotenv()
+
+llm1= HuggingFaceEndpoint(
+    repo_id="openai/gpt-oss-20b",
+    task= "text-generation"
+)
+
+model= ChatHuggingFace(
+    llm= llm1
+)
+
+parser= StrOutputParser()
+
+prompt=PromptTemplate(
+    template="Answer the following question {question} from the following text {text}",
+    input_variables=['question','text']
+)
+uri='https://en.wikipedia.org/wiki/SpaceX'
+
+loader=WebBaseLoader(uri)
+docs= loader.load()
+
+chain = prompt | model | parser
+
+result=chain.invoke({'question':'how is the founder of Space X','text': docs[0].page_content})
+
+print(result)
